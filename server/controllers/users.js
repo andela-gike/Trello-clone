75decca6f046b6b57c3200f6ef3f5452ace9e9a9
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-04-25 15:07:36 +0100

    Adding controllers for the various components

---
+++ server/controllers/users.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const User = mongoose.model('User');
+
+exports.create = (req, res) => {
+  if (req.body.name && req.body.password && req.body.email) {
+    User.findOne({
+      email: req.body.email
+    }).exec((err, existingUser) => {
+      if (!existingUser) {
+        const user = new User(req.body);
+        user.provider = 'local';
+        user.save((err) => {
+          if (err) {
+            return res.render('/#!/signup?error=unknown', {
+              errors: err.errors,
+              user
+            });
+          }
+          req.logIn(user, (err) => {
+            if (err) {
+              return next(err);
+            }
+            return res.redirect('/#!/');
+          });
+        });
+      } else {
+        return res.redirect('/#!/signup?error=existinguser');
+      }
+    });
+  } else {
+    return res.redirect('/#!/signup?error=incomplete');
+  }
+};
