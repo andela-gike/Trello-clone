11336ff7263861a139835f741ebb520762cb5597
Ike Grace <grace.ike@andela.comzenith007git config --global user.name andela-gike>
2017-04-25 16:56:16 +0100

    Configuring webpack to work with application server

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from './webpack.config.dev';
 
 const app = express();
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler, {
+  hot: true,
+  pubilcPath: webpackConfig.output.publicPath,
+  noInfo: true
+}));
+app.use(webpackHotMiddleware(compiler));
+
 //Set up default mongoose connection
 const mongoDB = 'mongodb://localhost/Trello-clone';
 mongoose.connect(mongoDB);
@@ -13,9 +28,9 @@ const db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.get('/', function (req, res) {
-  res.send('You are connected to Trello clone');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/index.html'));
 });
 
-app.listen(5000);
-console.log('Running on port 5000....')
+app.listen(5000, () => console.log('Running on port 5000....'));
+
